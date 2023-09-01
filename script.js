74966c66c434d123aef210449df56ccaba8736df
AMIR4SHARIF <amir4sharif@gmail.com>
2023-09-01 11:39:31 -0500

    few improvments

--- script.js
+++ script.js
@@ -23,41 +23,42 @@ function updateTime() {
   } setInterval(updateTime, 1000); // Update every second
 
   // LOGIN
-  document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById("login-form");
-  
-    loginForm.addEventListener("submit", async function (event) {
-      event.preventDefault();
-  
-      const username = loginForm.elements.LOGIN_USERNAME.value;
-      const password = loginForm.elements.LOGIN_PASS.value;
-  
-      const formData = {
-        username: username,
-        password: password,
-      };
-  
-      try {
-        const response = await fetch("http://185.230.163.241:8000/login/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        });
-  
-        if (response.ok) {
-          const data = await response.json();
-          const token = data.token;
-          window.localStorage.setItem("token", token); // Store the token in localStorage
-  
-          // Redirect to the dashboard page
-          window.location.href = "../../index.html";
-        } else {
-          console.error("Login failed");
-        }
-      } catch (error) {
-        console.error("An error occurred:", error);
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("login-form");
+
+  loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const username = loginForm.elements.LOGIN_USERNAME.value;
+    const password = loginForm.elements.LOGIN_PASS.value;
+
+    const formData = {
+      username: username,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("http://185.230.163.241:8000/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json", // Add this line to specify JSON response
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const token = data.token;
+        window.localStorage.setItem("token", token); // Store the token in localStorage
+
+        // Redirect to the dashboard page
+        window.location.href = "../dashboard";
+      } else {
+        console.error("Login failed");
       }
-    });
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
   });
+});
